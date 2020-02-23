b6b0c95688102ca563f0a0baba785109456988c2
smileyheart0@gmail.com <smileyheart02gmail.com>
2020-02-22 17:42:57 -0800

    HelloWorld

---
+++ Assignments/HelloWorld/main.cpp
@@ -0,0 +1,65 @@
+/* 
+ * File:   main.cpp
+ * 
+ * Author: Omar Alkendi
+ *
+ * Created on February 22, 2020, 5:26 PM
+ * 
+ * Purpose: 
+ * 
+ */
+
+//System Level Libraries
+#include <cstdlib>      //C Standard Library
+#include <iostream>     //I/O Library
+using namespace std; //Library Scope
+
+//User Libraries
+
+
+//Global Constants
+
+
+//Science and Math, Conversions, Higher Dimensions
+
+
+//constants to follow
+
+
+//Function Prototypes
+
+
+//Execution Starts Here
+
+int main(int argc, char** argv) {
+
+    //Set Random Number Seed Here
+
+
+
+    //Declare Variables - Known and Unknown, units, range, description
+
+
+
+    //Initialize Variables
+
+
+
+    //Map inputs to outputs -> i.e. process the inputs
+
+
+
+    //Display the outputs
+    cout << "Hello World!" << endl;
+    
+    
+    //Clean up - File closing, memory deallocation, etc....
+
+
+
+    //Exit Stage Right!
+
+    return 0;
+}
+
+//Function Implementations
